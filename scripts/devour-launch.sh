99c23d0dd3cec8878c1a78985c64d90aa17bd32b
joseph <josephg2044@gmail.com>
2022-10-26 18:39:37 -0400

    modified:   bspwm/bspwmrc 	deleted:    dwm 	modified:   nvim/lua/user/keymaps.lua 	modified:   polybar/config.ini 	modified:   rofi/big.rasi 	modified:   rofi/config.rasi 	modified:   scripts/devour-launch.sh 	new file:   scripts/gaptoggle 	new file:   scripts/homewifi1 	new file:   scripts/homewifi2 	new file:   scripts/kbtoggle 	modified:   scripts/polytoggle 	new file:   scripts/schoolwifi 	modified:   sxhkd/sxhkdrc 	modified:   zsh/.zcompdump 	modified:   dmenu (modified content, untracked content) 	modified:   dmenu-flexipatch (modified content, untracked content) 	modified:   polybar/polybar-vpn-controller (modified content)

--- scripts/devour-launch.sh
+++ scripts/devour-launch.sh
@@ -7,12 +7,15 @@
 case $(file --mime-type "$@" -bL) in
    # Check for the mimetype of your file (This is POSIX regex)
    video/* | audio/* | image/gif)
-      # Launch using your favorite application
-      devour mpv "$@"
-      ;;
+        # Launch using your favorite application
+        devour mpv "$@"
+        ;;
    # So on and so forth...
    application/pdf | application/postscript)
-      devour zathura "$@"
-      ;;
+        devour zathura "$@"
+        ;;
+    image/png | image/jpeg)
+        devour sxiv "$@"
+        ;;
    *) exit 1 ;;
 esac
