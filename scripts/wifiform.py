b3b27cd4f3ed41ef0481caeebd263689e94a9e61
joseph <josephg2044@gmail.com>
2022-11-09 15:03:34 -0500

    deleted:    scripts/geckodriver.log 	deleted:    scripts/homewifi1 	deleted:    scripts/homewifi2 	deleted:    scripts/schoolwifi 	deleted:    scripts/wifiform.py

--- scripts/wifiform.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-def main():
-    browser = webdriver.Firefox()
-    browser.get("https://lms.fcps.edu")
-    browser.maximize_window()
-
-if __name__ == "__main__":
-    main()
