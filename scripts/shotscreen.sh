f9ccf47e3723aa2a42f71e57ce5fc677301b30e1
Joseph <josephg2044@gmail.com>
2023-08-27 17:31:09 -0400

    asdf

--- scripts/shotscreen.sh
+++ scripts/shotscreen.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Screenshot
-time=`date +%H-%M-%S`
-day=`date +%Y-%m-%d`
-geometry=`xrandr | grep 'current' | head -n1 | cut -d',' -f2 | tr -d '[:blank:],current'`
-dir="`xdg-user-dir PICTURES`/screenshots/${day}"
+time=$(date +%H-%M-%S)
+day=$(date +%Y-%m-%d)
+geometry=$(xrandr | grep 'current' | head -n1 | cut -d',' -f2 | tr -d '[:blank:],current')
+dir="$(xdg-user-dir PICTURES)/screenshots/${day}"
 file="${time}_${geometry}.png"
 
 if [[ ! -d "$dir" ]]; then
