4b139a8988d186ccd766dcb4f80d6a62055667c8
joseph <josephg2044@gmail.com>
2024-02-09 23:24:59 -0500

    asdf

--- scripts/shotarea.sh
+++ scripts/shotarea.sh
@@ -3,18 +3,14 @@
 # Screenshot
 time=`date +%H-%M-%S`
 day=`date +%Y-%m-%d`
-geometry=`xrandr | grep 'current' | head -n1 | cut -d',' -f2 | tr -d '[:blank:],current'`
-dir="`xdg-user-dir PICTURES`/screenshots/${day}"
-file="${time}_${geometry}.png"
+dir="/home/joseph/pictures/screenshots/${day}"
+file="${time}.png"
 
 if [[ ! -d "$dir" ]]; then
 	mkdir -p "$dir"
 fi
 
 # Copy screenshot to clipboard
-copy_shot () {
-	tee "$file" | xclip -selection clipboard -t image/png
-}
-
-cd ${dir} && maim -u -m 10 -f png -s -b 1 -c 0.75,0.75,0.85,0.15 -l | copy_shot
+cd ${dir}; shotgun -g "$(hacksaw)" $file
+xclip -t 'image/png' -selection clipboard -i $file
 nsxiv $file
--- scripts/shotscreen.sh
+++ scripts/shotscreen.sh
@@ -1,32 +1,16 @@
 #!/usr/bin/env bash
 
 # Screenshot
-time=$(date +%H-%M-%S)
-day=$(date +%Y-%m-%d)
-geometry=$(xrandr | grep 'current' | head -n1 | cut -d',' -f2 | tr -d '[:blank:],current')
-dir="$(xdg-user-dir PICTURES)/screenshots/${day}"
-file="${time}_${geometry}.png"
+time=`date +%H-%M-%S`
+day=`date +%Y-%m-%d`
+dir="/home/joseph/pictures/screenshots/${day}"
+file="${time}.png"
 
 if [[ ! -d "$dir" ]]; then
 	mkdir -p "$dir"
 fi
 
-# notify and view screenshot
-notify_view() {
-	notify_cmd_shot='dunstify -u low --replace=699'
-	${notify_cmd_shot} "Copied to clipboard."
-	viewnior ${dir}/"$file"
-	if [[ -e "$dir/$file" ]]; then
-		${notify_cmd_shot} "Screenshot Saved."
-	else
-		${notify_cmd_shot} "Screenshot Deleted."
-	fi
-}
-
 # Copy screenshot to clipboard
-copy_shot () {
-	tee "$file" | xclip -selection clipboard -t image/png
-}
-
-cd ${dir} && sleep 0.1 && maim -u -m 10 -f png | copy_shot
+cd ${dir}; shotgun $file
+xclip -t 'image/png' -selection clipboard -i $file
 nsxiv $file
